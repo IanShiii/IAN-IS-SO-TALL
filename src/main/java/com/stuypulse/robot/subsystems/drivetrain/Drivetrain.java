2fd8d34ca9206de6807c912b76427f05f72d60ac
Ian <ianshi1026@gmail.com>
2023-05-18 20:11:01 -0400

    fixed errors

--- src/main/java/com/stuypulse/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/com/stuypulse/robot/subsystems/drivetrain/Drivetrain.java
@@ -3,5 +3,5 @@ package com.stuypulse.robot.subsystems.drivetrain;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    DifferentialDrive
+    
 }
--- src/main/java/com/stuypulse/robot/subsystems/drivetrain/DrivetrainImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/drivetrain/DrivetrainImpl.java
@@ -1,7 +1,5 @@
 package com.stuypulse.robot.subsystems.drivetrain;
 
-import org.apache.commons.io.comparator.DirectoryFileComparator;
-
 public class DrivetrainImpl extends Drivetrain{
     
 }
